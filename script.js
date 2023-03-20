80b6ce7ecacfb76ea9dbc789e8887992fc8f6728
nickhoban <110017566+nickhoban@users.noreply.github.com>
2023-03-20 17:24:20 +0800

    Added basic Javascript function

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// Empty script file to start with
+//Step 1 find the element
+var addTaskButton = document.getElementById("add-task");
+var newTaskInput = document.getElementById("task-input");
+var todoListContainer = document.getElementById("todo-list");
+
+//Step 2 write the behavour 
+
+function addTaskClicked(event) {
+    var taskName = newTaskInput.value;
+    newTaskInput.value ="";
+    todoListContainer.insertAdjacentHTML('afterbegin', taskName);
+}
+
+//Step 3 link to event handler 
+
+addTaskButton.addEventListener('click', onAddTaskClicked);
+
