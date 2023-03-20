19abf84c048269a480b3c68799d95333b2b1ad86
nickhoban <110017566+nickhoban@users.noreply.github.com>
2023-03-20 17:46:33 +0800

    Added function ontodo list clicked mark as completed

--- script.js
+++ script.js
@@ -18,6 +18,12 @@ function onTodoListContainerClicked(event) {
     while (!targetElement.classList.contains("task")){
         targetElement = targetElement.parentElement;
     } 
+    var checkbox = targetElement.querySelector(".checkbox");
+    if(checkbox.checked){
+        targetElement.classList.add("completed")
+    } else {
+        targetElement.classList.remove("completed")
+    }
 }
 
 //Step 3 link to event handler 
