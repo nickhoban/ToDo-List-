f849446cadb977cf207f9670b8ff719debb79466
nickhoban <110017566+nickhoban@users.noreply.github.com>
2023-03-27 17:15:02 +0800

    Javascript Updates

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ var templateContainer = document.getElementById("list-item-template");
 var template = templateContainer.innerHTML
 
 var showActiveButton = document.getElementById("show-active");
+var showAllTaskButton = document.getElementById("show-all");
+var showCompletedBotton = document.getElementById("show-completed");
 
 //Step 2 write the behavour 
 
@@ -41,12 +43,35 @@ function showActiveTasks() {
             task[i].style.display = "block";
         }
     }
-    
+}
+
+function showAllTasks() {
+    var tasks = document.getElementsByClassName('task'):
+    for (let i = 0; i < task.length; i++){
+        task[i].stle.display = 'block'
+    }
+}
+
+function showCompletedTasks() {
+    var tasks = document.getElementsByClassName('task')
+    for (let i = 0; i < tasks.length; i++){
+        if (tasks[i].classList.contains("completed")){
+            //Get the document you want to hide
+            var element = task[i]
+
+            //Set the diplsay property to "none"
+            task[i].style.display = "none";
+        } else {
+            task[i].style.display = "block";
+        }
+    }
 }
 
 //Step 3 link to event handler 
 
 addTaskButton.addEventListener('click', onAddTaskClicked);
 todoListContainer.addEventListener('click', onTodolistClicked);
-showActiveButton.addEventListener('click', showActiveTasks)
+showActiveButton.addEventListener('click', showActiveTasks);
+showAllTaskButton.addEventListener('click', showAllTasks);
+showCompletedBotton.addEventListener('click', showCompletedTasks);
 
